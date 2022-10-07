50631a6e9c679f940833b75e0fcfb1269935a789
Ilia <inkognito_ilya@mail.ru>
2022-10-07 16:14:26 +0300

    feat: make blockWrongTimeForReturnSelect function

--- index.js
+++ index.js
@@ -59,6 +59,30 @@ const convertTime = (timeArray) => {
 convertTime(startTimeFromA);
 convertTime(startTimeFromB);
 
+const blockWrongTimeForReturnSelect = () => {
+  const returnTimeSelect = document.getElementById('return-time');
+  
+  if (returnTimeSelect) {
+    returnTimeSelect.addEventListener('focus', () => {
+      const startTime = document.getElementById('start-time').value.split(':');
+      const options = returnTimeSelect.children;
+
+      for (let i = 0; i < options.length; i += 1) {
+        const returnTime = options[i].value.split(':');
+        const startDate = new Date();
+        const returnDate = new Date();
+
+        startDate.setHours(Number(startTime[0]), Number(startTime[1]));
+        returnDate.setHours(Number(returnTime[0]), Number(returnTime[1]));
+
+        if (startDate.getTime() + (CONSTS.ONE_DIRECTION_TIME * 60000) > returnDate.getTime()) {
+          options[i].setAttribute('disabled', 'true');
+        }
+      }
+    });
+  }
+}
+
 const direction = document.getElementById('route');
 
 direction.addEventListener('change', () => {
@@ -116,6 +140,8 @@ direction.addEventListener('change', () => {
   }
 
   document.getElementById('route').insertAdjacentElement('afterend', newSpan);
+
+  blockWrongTimeForReturnSelect();
 });
 
 const getTicketsQuantity = () => {
